b4bd84da9a2b9740fff1870039600325cfa2feea
Serhii <sergey.melnichuk92@gmail.com>
2021-08-13 10:54:38 +0300

    get movies ids

--- movies-project/src/store/modules/movies.js
+++ movies-project/src/store/modules/movies.js
@@ -1,13 +1,30 @@
 import axios from "@/plugins/axios";
+import IDs from "@/store/mock/imdb_top";
 const moviesStore = {
   namespaced: true,
-  state: {},
-  getters: {},
+  state: {
+    top250IDs: IDs,
+    moviesPrePage: 12,
+    currentPage: 1,
+  },
+  getters: {
+    slisedIds: ({ top250IDs }) => (from, to) =>top250IDs.slice(from,to),
+    currentPage: ({ currentPage }) => currentPage,
+    moviesPrePage: ({ moviesPrePage }) => moviesPrePage,
+  },
   mutations: {},
   actions: {
-    async fetchMovies(context) {
-      console.log(context);
-      const responce = await axios.get("/?i=tt0111161");
+    async fetchMovies({ getters }) {
+      const {currentPage, moviesPrePage, slisedIds} = getters;
+      const from = currentPage * moviesPrePage - moviesPrePage;
+      const to = currentPage * moviesPrePage;
+      const moviesTofetch = slisedIds(from, to);
+      console.log(moviesTofetch);
+      const responce = await axios.get("/", {
+        params: {
+          i: "?i=tt0111161"
+        },
+      });
       console.log(responce);
     },
   },
