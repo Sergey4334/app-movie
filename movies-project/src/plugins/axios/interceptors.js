3247e94930542bec0fff791b304b0cf5e287d1b9
Serhii <sergey.melnichuk92@gmail.com>
2021-08-18 12:00:09 +0300

    finished movies to fetch

--- movies-project/src/plugins/axios/interceptors.js
+++ movies-project/src/plugins/axios/interceptors.js
@@ -6,7 +6,10 @@ function setParams(config) {
   })
   return config;
 }
-
+function returnData(res) {
+  return res.data;
+}
 export default function(axios) {
-  axios.interceptors.request.use(setParams)
+  axios.interceptors.request.use(setParams);
+  axios.interceptors.response.use(returnData);
 }
--- movies-project/src/store/modules/movies.js
+++ movies-project/src/store/modules/movies.js
@@ -1,31 +1,48 @@
 import axios from "@/plugins/axios";
 import IDs from "@/store/mock/imdb_top";
+import mutations from "@/store/mutations";
+
+function normalizeResponse(movies) {
+  return movies.reduce((acc, movie) =>{
+    acc[movie.imdbID] = movie;
+    return acc;
+  }, {});
+}
+
+const { MOVIES } = mutations;
+
 const moviesStore = {
   namespaced: true,
   state: {
     top250IDs: IDs,
     moviesPrePage: 12,
     currentPage: 1,
+    movies: {},
   },
   getters: {
     slisedIds: ({ top250IDs }) => (from, to) =>top250IDs.slice(from,to),
     currentPage: ({ currentPage }) => currentPage,
     moviesPrePage: ({ moviesPrePage }) => moviesPrePage,
   },
-  mutations: {},
+  mutations: {
+    [MOVIES](state, value) {
+      state.movies = value;
+    }
+  },
   actions: {
-    async fetchMovies({ getters }) {
-      const {currentPage, moviesPrePage, slisedIds} = getters;
-      const from = currentPage * moviesPrePage - moviesPrePage;
-      const to = currentPage * moviesPrePage;
-      const moviesTofetch = slisedIds(from, to);
-      console.log(moviesTofetch);
-      const responce = await axios.get("/", {
-        params: {
-          i: "?i=tt0111161"
-        },
-      });
-      console.log(responce);
+    async fetchMovies({ getters, commit }) {
+      try {
+        const {currentPage, moviesPrePage, slisedIds} = getters;
+        const from = currentPage * moviesPrePage - moviesPrePage;
+        const to = currentPage * moviesPrePage;
+        const moviesTofetch = slisedIds(from, to);
+        const requests = moviesTofetch.map(id => axios.get(`/?i=${ id }`));
+        const response = await Promise.all(requests);
+        const movies = normalizeResponse(response);
+        commit(MOVIES, movies);
+      } catch(error) {
+        console.log(error);
+      }
     },
   },
 };
